d38091ff821f73f26d68cff63cd863978d8d804e
rodney <rjkahane@gmail.com>
2018-07-25 15:15:24 -0400

    update

---
+++ graphs/src/graphs/Graph.java
@@ -0,0 +1,38 @@
+package graphs;
+
+import java.util.ArrayList;
+
+public class Graph {
+	private int vCount;
+	private int eCount;
+	
+	private ArrayList[] adjacents;
+	
+	public Graph(int vCount) {
+		this.vCount = vCount;
+		this.eCount = 0;
+		adjacents = new ArrayList [vCount];
+		
+		for(int i=0;i<vCount;i++) {
+			adjacents[i] = new ArrayList<Integer>();
+		}
+	}//end constructor
+	
+	public int getVertexCount() {
+		return vCount;
+	}
+	
+	public int getEdgeCount() {
+		return eCount;
+	}
+	
+	public void addEdge(int src, int dest) {
+		adjacents[src].add(dest);
+		eCount++;
+	}
+	
+	public Object[] adj(int src) {
+		return adjacents[src].toArray();
+	}
+	
+}//end class
